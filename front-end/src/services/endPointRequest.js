acb22def6e68d8c5461d033377b5d7930add0902
Daniel Yabu <danielyabu@yahoo.com>
2023-02-14 15:29:47 -0300

    Alterado baseURL para link do deploy do back end

--- front-end/src/services/endPointRequest.js
+++ front-end/src/services/endPointRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const endpointRequest = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://delivery-deploy-production.up.railway.app/',
 });
 
 export const signLogin = async (endpoint, body) => {
